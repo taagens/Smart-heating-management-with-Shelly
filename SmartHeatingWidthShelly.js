a4688286cdd6abc8b3e352fc6b628bb7fb350980
taagens <taago.vaiku@gmail.com>
2024-12-29 14:05:49 +0200

    Update SmartHeatingWidthShelly.js
    Pelleti 15kg koti hind u. 3,6 ja katla efektiivsus 70% teeb pelleti kWh hinnaks 77EUR/MWh

--- SmartHeatingWidthShelly.js
+++ SmartHeatingWidthShelly.js
@@ -42,7 +42,7 @@ heatingMode.isFcstUsed: true/false - Using weather forecast to calculate heating
 let s = {
     heatingMode: { timePeriod: 12, heatingTime: 0, isFcstUsed: false }, // HEATING MODE. Different heating modes described above.
     elektrilevi: "VORK4",      // ELEKTRILEVI/IMATRA transmission fee: VORK1 / VORK2 / VORK4 /VORK5 / Partner24 / Partner24Plus / Partner12 / Partner12Plus / NONE
-    alwaysOnLowPrice: 110,       // Keep heating always ON if energy price lower than this value (EUR/MWh)
+    alwaysOnLowPrice: 77,       // Keep heating always ON if energy price lower than this value (EUR/MWh)
     alwaysOffHighPrice: 111,    // Keep heating always OFF if energy price higher than this value (EUR/MWh)
     isOutputInverted: false,    // Configures the relay state to either normal or inverted. (inverted required by Nibe, Thermia)
     relayID: 0,                 // Shelly relay ID
